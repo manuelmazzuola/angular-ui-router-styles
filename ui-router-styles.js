c645ee4121688c71fdc16323163a303ecd29fb60
Manuel Mazzuola <manuel.mazzuola@cloudesire.com>
2015-06-11 12:22:33 +0200

    Refactored code

--- ui-router-styles.js
+++ ui-router-styles.js
@@ -4,47 +4,61 @@
  * Inspired by https://github.com/tennisgent/angular-route-styles
  */
 
-'use strict';
+(function() {
+  'use strict';
+  angular
+    .module('uiRouterStyles', ['ui.router'])
+    .directive('head', uiRouterStylesDirective);
 
-angular
-  .module('uiRouterStyles', ['ui.router'])
-  .directive('head', ['$rootScope', '$compile', '$state', '$interpolate',
-    function($rootScope, $compile, $state, $interpolate) {
-      return {
-        restrict: 'E',
-        link: function(scope, elem){
-          var start = $interpolate.startSymbol(),
-              end = $interpolate.endSymbol();
-          var html = '<link rel="stylesheet" ng-repeat="(k, css) in routeStyles track by k" ng-href="' + start + 'css' + end + '" >';
-          elem.append($compile(html)(scope));
+  uiRouterStylesDirective.$inject = ['$rootScope', '$compile', '$state', '$interpolate'];
+  function uiRouterStylesDirective($rootScope, $compile, $state, $interpolate) {
+    var directive = {
+      restrict: 'E',
+      link: uiRouterStylesLink
+    };
 
-          // Get the parent state
-          var $$parentState = function(state) {
-            // Check if state has explicit parent OR we try guess parent from its name
-            var name = state.parent || (/^(.+)\.[^.]+$/.exec(state.name) || [])[1];
-            // If we were able to figure out parent name then get this state
-            return name && $state.get(name);
-          };
+    return directive;
 
-          scope.routeStyles = [];
-          $rootScope.$on('$stateChangeSuccess', function (evt, toState) {
-            // From current state to the root
-            scope.routeStyles = [];
-            for(var state = toState; state && state.name !== ''; state=$$parentState(state)) {
-              if(state && state.data && state.data.css) {
-                if(!Array.isArray(state.data.css)) {
-                  state.data.css = [state.data.css];
-                }
-                angular.forEach(state.data.css, function(css) {
-                  if(scope.routeStyles.indexOf(css) === -1) {
-                    scope.routeStyles.push(css);
-                  }
-                });
-              }
+    function uiRouterStylesLink(scope, elem) {
+      var start = $interpolate.startSymbol(), end = $interpolate.endSymbol();
+      var html = '<link rel="stylesheet" ng-repeat="(k, css) in routeStyles track by k" ng-href="' + start + 'css' + end + '" >';
+
+      scope.routeStyles = [];
+
+      activate();
+
+      ////
+
+      function activate() {
+        elem.append($compile(html)(scope));
+        $rootScope.$on('$stateChangeSuccess', stateChangeSuccessCallback);
+      }
+
+      // Get the parent state
+      function $$parentState(state) {
+        // Check if state has explicit parent OR we try guess parent from its name
+        var name = state.parent || (/^(.+)\.[^.]+$/.exec(state.name) || [])[1];
+        // If we were able to figure out parent name then get this state
+        return name && $state.get(name);
+      }
+
+      function stateChangeSuccessCallback(evt, toState) {
+        // From current state to the root
+        scope.routeStyles = [];
+        for(var state = toState; state && state.name !== ''; state=$$parentState(state)) {
+          if(state && state.data && state.data.css) {
+            if(!Array.isArray(state.data.css)) {
+              state.data.css = [state.data.css];
             }
-            scope.routeStyles.reverse();
-          });
+            angular.forEach(state.data.css, function(css) {
+              if(scope.routeStyles.indexOf(css) === -1) {
+                scope.routeStyles.push(css);
+              }
+            });
+          }
         }
-      };
+        scope.routeStyles.reverse();
+      }
     }
-  ]);
+  }
+})();
